2dbe72bcc6b300d2bab3f815c3753a61c24d0fc1
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-09 11:22:10 -0300

    style: change styles of TaskItem component

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,14 +1,19 @@
 import "./TaskItem.css";
 import { Task, changeStatus, deleteTask } from "../../types";
+import { Button, Checkbox, Flex } from "@chakra-ui/react";
 
 const TaskItem = ({ task, deleteTask, changeStatus }: { task: Task, deleteTask: deleteTask, changeStatus: changeStatus }) => {
 
+  const styleCompleted = {
+    textDecoration: "line-through",
+    color: "blue"
+  }
+
   return (
-    <div className="task" id={task.id}>
-      <input type="checkbox" onChange={() => changeStatus(task.id)} />
-      <p className={`${task.isCompleted ? "completed" : ""}`}>{task.name}</p>
-      <button onClick={() => deleteTask(task.id)}>Delete</button>
-    </div>
+    <Flex id={task.id} bg="blue.100" justify="space-between" align="center" py="10px" px="15px" m="10px" rounded="7px">
+      <Checkbox onChange={() => changeStatus(task.id)} style={task.isCompleted ? styleCompleted : {}} >{task.name}</Checkbox>
+      <Button onClick={() => deleteTask(task.id)}>Delete</Button>
+    </Flex>
   )
 }
 
