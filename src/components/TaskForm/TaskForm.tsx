c19beaba10d863bb6437af6e8d61969e2156163e
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-12 12:13:36 -0300

    style: change styles of TaskForm component and events

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -2,6 +2,7 @@ import "./TaskForm.css";
 import { addTask } from "../../types";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { Box, Button, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 const TaskForm = ({ addTask }: { addTask: addTask }) => {
   const [inputValue, setInputValue] = useState("");
@@ -13,15 +14,21 @@ const TaskForm = ({ addTask }: { addTask: addTask }) => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    addTask({ id: uuidv4(), name: inputValue, isCompleted: false });
+    if (inputValue) {
+      setInputValue("");
+      addTask({ id: uuidv4(), name: inputValue, isCompleted: false });
+    }
   }
 
   return (
-    <form className="tasks-form" onSubmit={handleSubmit}>
-      <label htmlFor="newTask">Create a new task</label>
-      <input type="text" id="newTask" name="task" placeholder="Add task" onChange={handleChange} />
-      <button type="submit">Create</button>
-    </form>
+    <Box as="form" onSubmit={handleSubmit} bg="white" p="10px" my="10px" mx="15px" rounded="7px">
+      <InputGroup>
+        <InputLeftElement>
+          <Button type="submit" bg="transparent" _hover={{ backgroundColor: "tranparent" }} _focus={{ backgroundColor: "transparent" }}>+</Button>
+        </InputLeftElement>
+        <Input type="text" value={inputValue} border="none" focusBorderColor="transparent" autoComplete="off" id="newTask" name="task" placeholder="Add task" onChange={handleChange} />
+      </InputGroup>
+    </Box>
   )
 }
 
