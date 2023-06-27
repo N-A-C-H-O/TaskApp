b002bb294a0c9cd04b284afd7aefe017e14f6a14
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-27 17:09:05 -0300

    feat: add Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@chakra-ui/react";
+import TaskContainer from "./TaskContainer";
+import Navbar from "./Navbar";
+
+function Home() {
+  return (
+    <Grid templateColumns={{ md: "1fr 2fr", lg: "1fr 4fr", xl: "1fr 5fr" }} height="100vh">
+      <Navbar />
+      <TaskContainer />
+    </Grid>
+  );
+}
+
+export default Home;
