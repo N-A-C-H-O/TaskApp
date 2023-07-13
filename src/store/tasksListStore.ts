3ebab773456fdc8ac54e16bf1a8bdea696c84466
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-07-13 10:57:40 -0300

    fix: modify changeTaskStatus method of store and change return of undefined tasList

--- src/store/tasksListStore.ts
+++ src/store/tasksListStore.ts
@@ -3,94 +3,106 @@ import { Task, TaskList } from "../types";
 import { v4 as uuidv4 } from "uuid";
 
 interface TasksListState {
-    items: TaskList[];
-    selected: TaskList;
-    createOne: (taskList: TaskList) => void;
-    deleteOne: (id: string) => void;
-    selectOne: (id: string) => void;
-    addTask: (newTask: Task) => void;
-    deleteTask: (taskId: string) => void;
-    changeTaskStatus: (taskId: string) => void;
+  items: TaskList[];
+  selected: TaskList;
+  createOne: (taskList: TaskList) => void;
+  deleteOne: (id: string) => void;
+  selectOne: (id: string) => void;
+  addTask: (newTask: Task) => void;
+  deleteTask: (taskId: string) => void;
+  changeTaskStatus: (taskId: string) => void;
 }
 
 const HomeId = uuidv4();
 
 export const useTasksListStore = create<TasksListState>((set, get) => ({
-    items: [{
-        id: HomeId,
-        name: "Home",
-        totalTasks: 0,
-        tasks: []
-    }],
-    selected: {
-        id: HomeId,
-        name: "Home",
-        totalTasks: 0,
-        tasks: []
+  items: [
+    {
+      id: HomeId,
+      name: "Home",
+      totalTasks: 0,
+      tasks: [],
     },
-    createOne: (taskList) => set((state) => ({
-        items: [...state.items, taskList]
+  ],
+  selected: {
+    id: HomeId,
+    name: "Home",
+    totalTasks: 0,
+    tasks: [],
+  },
+  createOne: (taskList) =>
+    set((state) => ({
+      items: [...state.items, taskList],
     })),
-    deleteOne: (id) => {
-        const { items } = get();
-        const filter = items.filter((item) => item.id !== id);
-        set(() => ({
-            items: filter
-        }));
-    },
-    selectOne: (id) => {
-        const { items } = get();
-        const newSelected = items.find((item) => item.id === id);
-        set(() => ({
-            selected: newSelected
-        }));
-    },
-    addTask: (newTask) => {
-        const { items, selected } = get();
+  deleteOne: (id) => {
+    const { items } = get();
+    const filter = items.filter((item) => item.id !== id);
 
-        console.log(items, selected)
+    set(() => ({
+      items: filter,
+    }));
+  },
+  selectOne: (id) => {
+    const { items } = get();
+    const newSelected = items.find((item) => item.id === id);
+    set(() => ({
+      selected: newSelected,
+    }));
+  },
+  addTask: (newTask) => {
+    const { items, selected } = get();
 
-        const taskList = items.find((item) => item.id === selected.id);
+    const taskList = items.find((item) => item.id === selected.id);
 
-        if (taskList !== undefined) {
-            taskList.tasks = [...taskList.tasks, newTask];
-            taskList.totalTasks+= 1;
-        };
-
-        set(() => ({
-            selected: taskList
-        }));
-    },
-    deleteTask: (taskId) => {
-        const { items, selected } = get();
-
-        const taskList = items.find((item) => item.id === selected.id);
-
-        if (taskList !== undefined) {
-            const filter = taskList?.tasks.filter((item) => item.id !== taskId);
-            taskList.tasks = filter;
-            taskList.totalTasks-= 1;
-        }
-
-        set(() => ({
-            selected: taskList
-        }));
-    },
-    changeTaskStatus: (taskId) => {
-        const { selected } = get();
-        set((state) => ({
-            selected: {
-                ...selected,
-                tasks: state.selected.tasks.map((task) => {
-                    if (task.id === taskId) {
-                        return {
-                            ...task,
-                            isCompleted: !task.isCompleted
-                        }
-                    }
-                    return task;
-                })
-            }
-        }));
+    if (taskList === undefined) {
+      return;
     }
+
+    taskList.tasks = [...taskList.tasks, newTask];
+    taskList.totalTasks += 1;
+
+    set(() => ({
+      selected: taskList,
+    }));
+  },
+  deleteTask: (taskId) => {
+    const { items, selected } = get();
+
+    const taskList = items.find((item) => item.id === selected.id);
+
+    if (taskList === undefined) {
+      return;
+    }
+
+    const filter = taskList.tasks.filter((item) => item.id !== taskId);
+    taskList.tasks = filter;
+    taskList.totalTasks -= 1;
+
+    set(() => ({
+      selected: taskList,
+    }));
+  },
+  changeTaskStatus: (taskId) => {
+    const { items, selected } = get();
+
+    const taskList = items.find((item) => item.id === selected.id);
+
+    if (taskList === undefined) {
+      return;
+    }
+
+    taskList.tasks = taskList.tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      return task;
+    });
+
+    set(() => ({
+      selected: taskList,
+    }));
+  },
 }));
