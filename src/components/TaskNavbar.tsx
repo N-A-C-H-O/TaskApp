6fafefb19c537d46f47d5fc509a88e56dc1cd227
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-07-18 10:32:13 -0300

    fix: modify changeName in Home list

--- src/components/TaskNavbar.tsx
+++ src/components/TaskNavbar.tsx
@@ -8,16 +8,40 @@ const TaskNavbar = ({ changeBackground }: { changeBackground: (newBackground: st
 
   const { selected, changeName } = useTasksListStore();
 
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.target;
+
+    value && changeName(value);
+  };
+
   return (
     <Flex justify="space-between" align="center" w="85%" mx="auto" mt="20px">
       <Box width="40%" py="5px">
-        {
-          isEditing ? 
+        {selected.name === "Home" ? (
+          <Heading as="h3" fontSize={35} padding={0}>
+            {selected.name}
+          </Heading>
+        ) : isEditing ? (
           <>
-            <Input onChange={(e) => changeName(e.target.value)} onBlur={() => setIsEditing(false)} width="100%" placeholder={selected.name} color="white" border="none" fontSize={35} focusBorderColor="transparent" fontWeight="bold" padding={0}/>
-          </> :
-          <Heading as="h3" fontSize={35} padding={0} onClick={() => setIsEditing(true)}>{selected.name}</Heading>
-        }
+            <Input
+              onChange={handleChange}
+              maxLength={25}
+              onBlur={() => setIsEditing(false)}
+              width="100%"
+              placeholder={selected.name}
+              color="white"
+              border="none"
+              fontSize={35}
+              focusBorderColor="transparent"
+              fontWeight="bold"
+              padding={0}
+            />
+          </>
+        ) : (
+          <Heading as="h3" fontSize={35} padding={0} onClick={() => setIsEditing(true)}>
+            {selected.name}
+          </Heading>
+        )}
       </Box>
       <BackgroundSelector changeBackground={changeBackground} />
     </Flex>
