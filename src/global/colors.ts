5a8acd5bba4669861426ccc51de4da34d0d891b0
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-12-12 16:12:19 -0300

    feat: add colors in global folder

---
+++ src/global/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  lightest: '#ecfdfe',
+  lighter: '#cff7fc',
+  light: '#a9f0f9',
+  normal: '#6de1f3',
+  dark: '#28c9e6',
+  darker: '#0eadcd',
+  darkest: '#0f87a9',
+  deep: '#136f8b',
+  deeper: '#195971',
+  deepest: '#194d61',
+  background: '#0a3243'
+};
+
+export default colors;
